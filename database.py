43674a491516370f8614729eadf354f5abb3b9f2
дм <zamulatimofei@yandex.ru>
2024-03-26 13:34:20 +0300

    sql

--- database.py
+++ database.py
@@ -23,25 +23,28 @@ class Database:
         self.conn.commit()
         self.conn.close()
 
-    def add_new_session(self, chat_id):
-        # Увеличиваем количество сессий на 1 и устанавливаем начальное количество токенов в 500
-        self.cursor.execute("INSERT INTO sessions (chat_id, sessions_count, tokens) VALUES (?, ?, ?) ON CONFLICT(chat_id) DO UPDATE SET sessions_count = sessions_count + 1, tokens = 500", (chat_id, 1, 500))
+    def create_user_profile(self, chat_id):
+        # cоздаем "профиль" с 4 сессиями и 500 токенами в каждой сессии
+        self.cursor.execute("INSERT INTO users (chat_id, sessions_count, tokens) VALUES (?, ?, ?)", (chat_id, 4, 500))
+        self.conn.commit()
+        self.conn.close()
+
+    def reset_session(self, chat_id):
+        # уменьшаем количество сессий на 1 и сбрасываем кол-во токенов до 500
+        self.cursor.execute("UPDATE users SET sessions_count = sessions_count - 1, tokens = 500 WHERE chat_id = ?",
+                            (chat_id,))
         self.conn.commit()
         self.conn.close()
 
     def update_tokens_used(self, chat_id, tokens_used):
-        self.cursor.execute("UPDATE tokens_used SET tokens_used = ? WHERE chat_id = ?", (tokens_used, chat_id))
+        # обновляем общее количество использованных токенов
+        self.cursor.execute("UPDATE tokens_used SET total_tokens_used = total_tokens_used + ? WHERE chat_id = ?", (tokens_used, chat_id))
         self.conn.commit()
         self.conn.close()
 
     def get_tokens_used(self, chat_id):
-        self.cursor.execute("SELECT tokens_used FROM tokens_used WHERE chat_id = ?", (chat_id,))
-        result = self.cursor.fetchone()
-        return result[0] if result else 0
-        self.conn.close()
-
-    def check_sessions_count(self, chat_id):
-        self.cursor.execute("SELECT sessions_count FROM sessions WHERE chat_id = ?", (chat_id,))
+        # получение кол-ва использованных токенов
+        self.cursor.execute("SELECT total_tokens_used FROM tokens_used WHERE chat_id = ?", (chat_id,))
         result = self.cursor.fetchone()
         return result[0] if result else 0
         self.conn.close()
