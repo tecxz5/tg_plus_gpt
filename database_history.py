864e6fbc17cc3b2bbc69cbc776d9d4a5f54812f7
дм <zamulatimofei@yandex.ru>
2024-03-28 13:25:24 +0300

    история

--- database_history.py
+++ database_history.py
@@ -5,21 +5,20 @@ import datetime
 class History:
     def __init__(self, db_file):
         self.conn = self.create_connection(db_file)
-        self.create_table()
 
     def create_connection(self, db_file):
         conn = None
         try:
-            conn = sqlite3.connect(db_file)
+            conn = sqlite3.connect(db_file, check_same_thread=False)
             return conn
         except Error as e:
             print(e)
 
-    def create_table(self):
+    def create_table(self, user_id):
+        table_name = f"history_{user_id}"
         try:
-            sql_create_table = """ CREATE TABLE IF NOT EXISTS history (
+            sql_create_table = f""" CREATE TABLE IF NOT EXISTS {table_name} (
                                             id integer PRIMARY KEY,
-                                            user_id text NOT NULL,
                                             role text NOT NULL,
                                             message text NOT NULL,
                                             timestamp text NOT NULL
@@ -30,8 +29,12 @@ class History:
             print(e)
 
     def save_message(self, user_id, role, message):
-        sql = ''' INSERT INTO history(user_id,role,message,timestamp)
-                 VALUES(?,?,?,?) '''
-        cur = self.conn.cursor()
-        cur.execute(sql, (user_id, role, message, datetime.datetime.now().isoformat()))
-        self.conn.commit()
+        if role != 'system':
+            table_name = f"history_{user_id}"
+            # Используем f-строку для формирования запроса, корректно обрабатывая переменные
+            sql = f''' INSERT INTO {table_name}(role,message,timestamp)
+                    VALUES(?,?,?) '''
+            cur = self.conn.cursor()
+            # Используем параметризованный запрос для безопасной вставки значений
+            cur.execute(sql, (role, message, datetime.datetime.now().isoformat()))
+            self.conn.commit()
