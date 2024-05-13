2fddc34f9675ccbd6e039da5c826004aeb061b9a
akate <zamulatimofei@yandex.ru>
2024-05-13 17:37:50 +0300

    автозамена токена

--- auto_token.py
+++ auto_token.py
@@ -1,31 +1,66 @@
+import json
+import logging  # модуль для сбора логов
+import time  # модуль для работы со временем
+from datetime import datetime  # модуль для работы с датой и временем
 import requests
-import schedule
-import time
+# подтягиваем константы из config-файла
+from config import *
 
-def get_token() -> str:
+# настраиваем запись логов в файл
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='logs.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    encoding='utf-8')  # логер
+
+# получение нового iam_token
+def create_new_token():
     url = "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token"
-    headers = {"Metadata-Flavor": "Google"}
-    response = requests.request("GET", url, headers=headers)
-    return response.json()["access_token"]
+    headers = {
+        "Metadata-Flavor": "Google"
+    }
+    try:
+        response = requests.get(url=url, headers=headers)
+        if response.status_code == 200:
+            token_data = response.json()  # вытаскиваем из ответа iam_token
+            # добавляем время истечения iam_token к текущему времени
+            token_data['expires_at'] = time.time() + token_data['expires_in']
+            # записываем iam_token в файл
+            with open(IAM_TOKEN, "w") as token_file:
+                json.dump(token_data, token_file)
+            logging.info("Получен новый iam_token")
+        else:
+            logging.error(f"Ошибка получения iam_token. Статус-код: {response.status_code}")
+    except Exception as e:
+        logging.error(f"Ошибка получения iam_token: {e}")
 
-def update_config_file(file_path):
-    # Чтение содержимого файла
-    with open(file_path, 'r') as file:
-        content = file.read()
+# чтение iam_token и folder_id из файла
+def get_creds():
+    try:
+        # чтение iam_token
+        with open(IAM_TOKEN, 'r') as f:
+            file_data = json.load(f)
+            expiration = datetime.strptime(file_data["expires_at"][:26], "%Y-%m-%dT%H:%M:%S.%f")
+        # если срок годности истёк
+        if expiration < datetime.now():
+            logging.info("Срок годности iam_token истёк")
+            # получаем новый iam_token
+            create_new_token()
+    except:
+        # если что-то пошло не так - получаем новый iam_token
+        create_new_token()
 
-    # Получение нового токена
-    new_token = get_token()
+    # чтение iam_token
+    with open(IAM_TOKEN, 'r') as f:
+        file_data = json.load(f)
+        iam_token = file_data["access_token"]
 
-    # Замена старого токена на новый
-    content = content.replace('IAM_TOKEN = "old_token_value"', f'IAM_TOKEN = "{new_token}"')
+    # чтение folder_id
+    with open(FOLDER_ID, 'r') as f:
+        folder_id = f.read().strip()
 
-    # Перезапись файла с новым содержимым
-    with open(file_path, 'w') as file:
-        file.write(content)
+    return iam_token, folder_id
 
-schedule.every(8).hours.do(update_config_file, 'config.py')
-
-while True:
-    # Проверяем и выполняем задачи, которые должны быть выполнены
-    schedule.run_pending()
-    time.sleep(1)
+# чтение bot_token из файла
+def get_bot_token():
+    with open(TOKEN, 'r') as f:
+        return f.read().strip()
