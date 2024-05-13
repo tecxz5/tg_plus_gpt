d6b1f8eee17dad500d826105191a753c748035a0
akate <zamulatimofei@yandex.ru>
2024-05-13 18:01:20 +0300

    Тех. Поддержка

--- support/bot/bot.py
+++ support/bot/bot.py
@@ -1,5 +1,5 @@
 import telebot
-from config import TOKEN
+from config import *
 from database import Database
 
 bot = telebot.TeleBot(TOKEN)
@@ -7,7 +7,7 @@ db = Database('database.db')
 db.create_table_if_not_exists()
 
 def check_user_id_and_send_db(message):
-    if message.from_user.id == <>:  # Замените <> на ID пользователя
+    if message.from_user.id == {ID}:
         with open('database.db', 'rb') as db_file:
             bot.send_document(message.chat.id, db_file)
         bot.send_message(message.chat.id, 'Файл базы данных отправлен.')
